223f41de7625d9a9cbb3c350960e33ce8129b5be
marktop <agressorrrrr@gmail.com>
2024-04-04 15:43:55 +0200

    add firebase and login

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,18 +1,73 @@
 "use client";
 
+import { signInAuthUserWithEmailAndPassword } from "@/utils/firebase/firebase.utils";
 import { Button, TextField } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
+const defaultForm: LoginForm = {
+  email: "",
+  password: "",
+};
 
 const Page = () => {
-  const handleSubmit = (e: any) => {
-    //login
-    alert(`login`);
+  const [form, setForm] = useState(defaultForm);
+  const router = useRouter();
+
+  const { email, password } = form;
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      if (response) {
+        router.push("/admin");
+        alert("logged in");
+      }
+    } catch (e: any) {
+      alert("error");
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <TextField id="username" label="Username" focused />
-        <TextField id="password" label="Password" type="password" focused />
+        <TextField
+          id="email"
+          label="Email"
+          type="email"
+          name="email"
+          required
+          focused
+          value={email}
+          onChange={handleChange}
+        />
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          name="password"
+          required
+          focused
+          value={password}
+          onChange={handleChange}
+        />
         <Button type="submit">Login</Button>
       </form>
     </>
---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: "831221351615",
+  appId: "1:831221351615:web:73dffb397da8844e44320a",
+};
+
+initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+
+export const db = getFirestore();
+
+export const signInAuthUserWithEmailAndPassword = async (
+  email: string,
+  password: string
+) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = () => signOut(auth);
