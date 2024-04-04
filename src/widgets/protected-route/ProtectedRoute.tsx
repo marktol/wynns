9edcca0805901c08de1b5b042c9711c62dd37e2f
marktop <agressorrrrr@gmail.com>
2024-04-04 17:56:59 +0200

    fix hydration error

--- src/widgets/protected-route/ProtectedRoute.tsx
+++ src/widgets/protected-route/ProtectedRoute.tsx
@@ -1,22 +1,26 @@
 "use client";
 
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/contexts/auth.context";
+import { User } from "firebase/auth";
 
 interface Props {
   children: ReactNode | string | JSX.Element | JSX.Element[];
 }
 
 const ProtectedRoute = ({ children }: Props) => {
-  const { currentUser: user } = useAuth();
+  const [user, setUser] = useState<User | undefined | null>();
+  const { currentUser } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
-    if (!user) {
+    setUser(currentUser);
+
+    if (!currentUser) {
       router.push("/");
     }
-  }, [user, router]);
+  }, [currentUser, router]);
 
   return <>{!user ? <>loading</> : children}</>;
 };
